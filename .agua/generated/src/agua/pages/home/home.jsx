fb5c2b0a3bd20d095f28a2ca138026bd126149d4
Santiago Saenz Ariza <santiago@agua.app>
2024-01-24 17:13:36 -0500

    Home_menu_desktop_2

--- .agua/generated/src/agua/pages/home/home.jsx
+++ .agua/generated/src/agua/pages/home/home.jsx
@@ -26,9 +26,22 @@ const Home = () =>{
 <section custom-agua-item="layer" className={styles.homemenu}   >
 <section custom-agua-item="layer" className={styles.homemenudesktop}   >
 <section custom-agua-item="layer" className={styles['layer-14-87a09d4412024edf9d4d374637b06392']}   >
-<section custom-agua-item="layer" className={styles['layer-23-96c7ec2cb4e44fe4871e25fe4667f447']}   ></section>
-<section custom-agua-item="layer" className={styles['layer-24-9cb29b486c294de5bf7e051b477a9039']}   ></section>
-<section custom-agua-item="layer" className={styles['layer-25-e598ed483728499c8dc122cac7562be6']}   ></section>
+<section custom-agua-item="layer" className={styles['layer-23-96c7ec2cb4e44fe4871e25fe4667f447']}   >
+<p custom-agua-item="text" className={styles['text-7-1be88a869df2490d9c3ef0f8e8987c9f']}   >Type...</p>
+<p custom-agua-item="text" className={styles['text-8-8fced5db04e3454086bc3bb5336e473d']}   >Type...</p>
+<p custom-agua-item="text" className={styles['text-9-406739ddf9284d7e80abdbb44e1e1eba']}   >Type...</p>
+</section>
+<section custom-agua-item="layer" className={styles['layer-24-9cb29b486c294de5bf7e051b477a9039']}   >
+<img custom-agua-item="imageView"   alt="imageview-2" src='https://i.ibb.co/5ndGgWv/image-43.png'  />
+</section>
+<section custom-agua-item="layer" className={styles['layer-25-e598ed483728499c8dc122cac7562be6']}   >
+<p custom-agua-item="text" className={styles['text-10-050fec7cb1fd4677b5b6090f94d71962']}   >Type...</p>
+<span custom-agua-item="icon"   >favorite</span>
+<section custom-agua-item="layer" className={styles['layer-26-503c3e91ce524f208d4fe958b3be2b2a']}   >
+<span custom-agua-item="icon"   >favorite</span>
+<span custom-agua-item="icon"   >favorite</span>
+</section>
+</section>
 </section>
 <section custom-agua-item="layer" className={styles['layer-15-50b404c305c34c4aa25f534d7851e9c9']}   ></section>
 </section>
